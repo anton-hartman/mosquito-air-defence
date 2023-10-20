152b1a862e3bba39dbff0f9e7ced26a19ab4ac03
Anton Hartjies <antonhartman14@gmail.com>
2023-10-20 13:00:47 +0200

    Added files from tracking branch before merge mess up.

---
+++ include/tracking.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include "frame.hpp"
+#include "hungarian.hpp"
+#include "mos.hpp"
+
+class Tracking {
+ private:
+  std::vector<Mos> mos_trackers;
+  HungarianAlgorithm hungarian;
+
+  void add_kalman();
+  std::vector<std::vector<double>> get_cost_matrix(
+      const std::vector<Pt>& blob_centres);
+  double euclidean_distance(const Pt_d& predicted, const Pt& detected);
+
+ public:
+  /**
+   * @brief Associate detections with predictions and update trackers
+   */
+  void associate_and_update(const std::vector<Pt>& blob_centres);
+  void predict_centres();
+  std::vector<Pt> get_predicted_centres() const;
+  Pt get_predicted_centre(const int& id) const;
+};
